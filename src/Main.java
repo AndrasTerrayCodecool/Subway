155d369399e23dcc9e56f7dd43456012ee584416
Andras Terray <andras.terray@codecool.com>
2024-02-12 11:00:09 +0100

    Moving main logic from Restaurant to the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import codecool.com.subway.Restaurant;
+import codecool.com.subway.orderitem.drink.Drink;
+import codecool.com.subway.orderitem.drink.Flavor;
+import codecool.com.subway.orderitem.sandwich.Ingredient;
+import codecool.com.subway.orderitem.sandwich.Sandwich;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Restaurant subway = new Restaurant("Subway");
+        Drink drink = new Drink(Flavor.COCA_COLA);
+        Ingredient ingredient1 = new Ingredient("Cheese", 100);
+        Ingredient ingredient2 = new Ingredient("Meatball", 120);
+        Sandwich sandwich = new Sandwich(ingredient1, ingredient2);
+        subway.addOrder(drink, sandwich);
+
+        System.out.println(subway);
+        System.out.println("Income for today in Subway: " + subway.calculateIncome());
     }
 }
--- src/codecool/com/subway/Restaurant.java
+++ src/codecool/com/subway/Restaurant.java
@@ -35,16 +35,4 @@ public class Restaurant {
                 '}';
     }
 
-    public static void main(String[] args) {
-        Restaurant subway = new Restaurant("Subway");
-        Drink drink = new Drink(Flavor.COCA_COLA);
-        Ingredient ingredient1 = new Ingredient("Cheese", 100);
-        Ingredient ingredient2 = new Ingredient("Meatball", 120);
-        Sandwich sandwich = new Sandwich(ingredient1, ingredient2);
-        subway.addOrder(drink, sandwich);
-
-        System.out.println(subway);
-        System.out.println("Income for today in Subway: " + subway.calculateIncome());
-    }
-
 }
